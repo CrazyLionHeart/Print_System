cedc5cab95dcbb3776e944450cd00f76899ef838
avsytar <avsytar@ubuntu.(none)>
2014-04-15 17:39:43 +0400

    Fix load config

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os import environ
+from os import environ, path
 import json
 
 current_env = environ.get("APPLICATION_ENV", 'development')
 
-with open('./%s/config.%s.json' % (current_env, current_env)) as f:
+with open('%s/%s/config.%s.json' % (path.dirname(path.abspath(__file__)), current_env, current_env)) as f:
     own_config = json.load(f)
 
 gunicorn = own_config['gunicorn']
