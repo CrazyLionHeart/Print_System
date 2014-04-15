b04760a3b3dcf50e6502dc434b55a2b0b6036f9a
Aleksandr Sytar <avsytar@babypages.ru>
2014-04-15 16:36:52 +0400

    Исправил местоположение демона печати

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -9,10 +9,12 @@ current_env = environ.get("APPLICATION_ENV", 'development')
 with open('./config/%s/config.%s.json' % (current_env, current_env)) as f:
     config = json.load(f)
 
-    bind = '%s:%s' % (config["gunicorn"]["hostname"],
-                      int(config["gunicorn"]["port"]))
-    workers = int(config["gunicorn"]["workers"])
-    worker_class = config["gunicorn"]["worker_class"]
-    worker_connections = int(config["gunicorn"]["worker_connections"])
-    timeout = int(config["gunicorn"]["timeout"])
-    keepalive = int(config["gunicorn"]["keepalive"])
+gunicorn = config['gunicorn']
+
+bind = '%s:%s' % (gunicorn["hostname"],
+                  int(gunicorn["port"]))
+workers = int(gunicorn["workers"])
+worker_class = gunicorn["worker_class"]
+worker_connections = int(gunicorn["worker_connections"])
+timeout = int(gunicorn["timeout"])
+keepalive = int(gunicorn["keepalive"])
