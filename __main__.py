141005a5b580c317bc2ef07fb68b51feffbdae2e
Aleksandr Sytar <avsytar@babypages.ru>
2014-04-15 13:37:43 +0400

    Почистил файл

--- __main__.py
+++ __main__.py
@@ -1,18 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from app import app
-from os import environ
-import json
-
-current_env = environ.get("APPLICATION_ENV", 'development')
-
-with open('config/%s/config.%s.json' % (current_env, current_env)) as f:
-    config = json.load(f)
-
-try:
-    port = int(config["Print_System"]["port"])
-    host = config["Print_System"]["hostname"]
-    app.run(host=host, port=port)
-except KeyboardInterrupt:
-        pass
