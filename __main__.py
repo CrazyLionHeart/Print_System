efbdc2f6b08e36d03dd9568489eb00a2063ca980
Aleksandr Sytar <avsytar@babypages.ru>
2014-04-15 12:57:28 +0400

    Добавил использование настроек

--- __main__.py
+++ __main__.py
@@ -7,12 +7,12 @@ import json
 
 current_env = environ.get("APPLICATION_ENV", 'development')
 
-with open('../../config/%s/config.%s.json' % (current_env, current_env)) as f:
+with open('config/%s/config.%s.json' % (current_env, current_env)) as f:
     config = json.load(f)
 
 try:
     port = int(config["Print_System"]["port"])
-    host = config['File_Storage']["hostname"]
+    host = config["Print_System"]["hostname"]
     app.run(host=host, port=port)
 except KeyboardInterrupt:
         pass
